27a8a249d0f66b6d990e95681c72af8984d36c3c
Michael Wu <mw10013@gmail.com>
2025-01-30 18:54:42 -0500

    d1-reset

--- functions/app/scripts/d1-reset.ts
+++ functions/app/scripts/d1-reset.ts
@@ -81,3 +81,7 @@ for (const wranglerJsoncPath of wranglerJsoncPaths) {
 	}
 	await fs.writeFile(wranglerJsoncPath, jsonc.applyEdits(wranglerJsonc, edit))
 }
+
+// Run migrations and seed the new database.
+await $`pnpm d1:migrate:apply:${env === 'production' ? 'PRODUCTION' : env}`
+// await $`pnpm d1:seed:PRODUCTION`
