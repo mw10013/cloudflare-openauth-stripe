5092b50fb6bd726d85caa5cb7de6c38acea182c9
Michael Wu <mw10013@gmail.com>
2025-01-30 17:17:03 -0500

    jsonc-parser

--- functions/app/scripts/hello.ts
+++ functions/app/scripts/hello.ts
@@ -1,1 +1,16 @@
-console.log('hello world')
+import 'zx/globals'
+import * as jsonc from 'jsonc-parser'
+import { $, fs } from 'zx'
+
+const env = argv.env || 'local'
+console.log({ argv: argv, env })
+
+const wranglerJsonc = await fs.readFile('wrangler.jsonc', 'utf-8')
+// const wranglerJson = jsonc.stripComments(wranglerJsonc)
+// const wrangler = JSON.parse(wranglerJson)
+
+// console.log({ wranglerJsonc, wranglerJson, wrangler })
+// console.dir(wrangler, { depth: null })
+
+const wrangler = jsonc.parse(wranglerJsonc)
+console.dir(wrangler, { depth: null })
