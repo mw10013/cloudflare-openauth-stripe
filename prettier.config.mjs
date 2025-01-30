ac19dc1d7d001890b72b4d679784ac7beec1a18b
Michael Wu <mw10013@gmail.com>
2025-01-30 13:30:47 -0500

    prettier sql

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -21,9 +21,20 @@ const config = {
 	tailwindFunctions: ['tv', 'composeTailwindRenderProps'],
 
 	plugins: [
+		'prettier-plugin-sql',
 		'@ianvs/prettier-plugin-sort-imports',
 		// https://github.com/tailwindlabs/prettier-plugin-tailwindcss?tab=readme-ov-file#compatibility-with-other-prettier-plugins
 		'prettier-plugin-tailwindcss' // MUST come last
+	],
+	overrides: [
+		{
+			files: ['*.sql'],
+			// https://github.com/un-ts/prettier/tree/master/packages/sql#parser-options
+			options: {
+				language: 'sqlite',
+				keywordCase: 'lower'
+			}
+		}
 	]
 }
 
