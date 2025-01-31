4d2820e1f5ea7e138947547ac318af9349fc0f67
Michael Wu <mw10013@gmail.com>
2025-01-31 11:20:12 -0500

    stripe secret key

--- functions/app/types/worker-configuration.d.ts
+++ functions/app/types/worker-configuration.d.ts
@@ -4,6 +4,8 @@ interface Env {
 	ENVIRONMENT: "local" | "staging" | "production";
 	OPENAUTH_ISSUER: "http://localhost:8788" | "https://cloudflare-openauth-stripe-worker-staging.devxo.workers.dev" | "https://cloudflare-openauth-stripe-worker-production.devxo.workers.dev";
 	COOKIE_SECRET: string;
+	STRIPE_SECRET_KEY: string;
+	STRIPE_WEBHOOK_SECRET: string;
 	D1: D1Database;
 	WORKER: Fetcher;
 	ASSETS: Fetcher;
