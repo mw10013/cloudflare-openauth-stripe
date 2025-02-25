5ce5069569d5be97afb43cbe5f7647ebcb29d060
Michael Wu <mw10013@gmail.com>
2025-02-25 16:12:50 -0500

    updateStripe

--- functions/app/src/Repository.ts
+++ functions/app/src/Repository.ts
@@ -6,8 +6,8 @@ export const make = Effect.gen(function* () {
 	const d1 = yield* D1 // Outside of functions so that D1 does not show up in R
 	return {
 		getTeams: () =>
-			Effect.gen(function* () {
-				const statement = d1.prepare(
+			pipe(
+				d1.prepare(
 					`
 select json_group_array(
 json_object(
@@ -25,9 +25,10 @@ from teamMembers tm where tm.teamId = t.teamId
 )
 ) as data from teams t
 `
-				)
-				return yield* d1.first(statement).pipe(Effect.flatMap(Schema.decodeUnknown(TeamsResult)))
-			}),
+				),
+				d1.first,
+				Effect.flatMap(Schema.decodeUnknown(TeamsResult))
+			),
 		getTeamForUser: ({ userId }: Pick<User, 'userId'>) =>
 			pipe(
 				d1
@@ -35,6 +36,28 @@ from teamMembers tm where tm.teamId = t.teamId
 					.bind(userId),
 				d1.first,
 				Effect.flatMap(Schema.decodeUnknown(TeamResult))
+			),
+		updateStripeCustomerId: ({ teamId, stripeCustomerId }: Pick<Team, 'teamId' | 'stripeCustomerId'>) =>
+			pipe(d1.prepare('update teams set stripeCustomerId = ? where teamId = ?').bind(stripeCustomerId, teamId), d1.run),
+		updateStripeSubscription: ({
+			stripeCustomerId,
+			stripeSubscriptionId,
+			stripeProductId,
+			planName,
+			subscriptionStatus
+		}: Pick<
+			{
+				[K in keyof Team]: NonNullable<Team[K]>
+			},
+			'stripeCustomerId' | 'stripeSubscriptionId' | 'stripeProductId' | 'planName' | 'subscriptionStatus'
+		>) =>
+			pipe(
+				d1
+					.prepare(
+						'update teams set stripeSubscriptionId = ?, stripeProductId = ?, planName = ?, subscriptionStatus = ? where stripeCustomerId = ?'
+					)
+					.bind(stripeSubscriptionId, stripeProductId, planName, subscriptionStatus, stripeCustomerId),
+				d1.run
 			)
 	}
 })
@@ -42,15 +65,3 @@ from teamMembers tm where tm.teamId = t.teamId
 export class Repository extends Effect.Tag('Repository')<Repository, Effect.Effect.Success<typeof make>>() {
 	static Live = Layer.effect(this, make)
 }
-
-// updateStripeCustomerId: async ({
-//   teamId,
-//   stripeCustomerId
-// }: Pick<
-//   {
-//     [K in keyof Team]: NonNullable<Team[K]>
-//   },
-//   'teamId' | 'stripeCustomerId'
-// >) => {
-//   await db.prepare('update teams set stripeCustomerId = ? where teamId = ?').bind(stripeCustomerId, teamId).run()
-// },
