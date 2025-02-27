912d28c689eddd0de6fbf2417a81930704f4816b
Michael Wu <mw10013@gmail.com>
2025-02-27 14:27:41 -0500

    getTeams sql

--- functions/app/src/Repository.ts
+++ functions/app/src/Repository.ts
@@ -9,22 +9,14 @@ export const make = Effect.gen(function* () {
 			pipe(
 				d1.prepare(
 					`
-select json_group_array(
-json_object(
-'teamId', teamId, 'name', name, 'stripeCustomerId', stripeCustomerId, 'stripeSubscriptionId', stripeSubscriptionId, 'stripeProductId', stripeProductId, 'planName', planName, 'subscriptionStatus', subscriptionStatus,
-'teamMembers',
-(
-select
-json_group_array(
-json_object(
-'teamMemberId', tm.teamMemberId, 'userId', tm.userId, 'teamId', tm.teamId, 'teamMemberRole', tm.teamMemberRole,
-'user', (select json_object('userId', u.userId, 'name', u.name, 'email', u.email, 'role', u.role) from users u where u.userId = tm.userId))
-)
-from teamMembers tm where tm.teamId = t.teamId
-)
-)
-) as data from teams t
-`
+select json_group_array(json_object(
+	'teamId', teamId, 'name', name, 'stripeCustomerId', stripeCustomerId, 'stripeSubscriptionId', stripeSubscriptionId, 'stripeProductId', stripeProductId, 'planName', planName, 'subscriptionStatus', subscriptionStatus,
+	'teamMembers',
+	(select json_group_array(json_object(
+		'teamMemberId', tm.teamMemberId, 'userId', tm.userId, 'teamId', tm.teamId, 'teamMemberRole', tm.teamMemberRole,
+		'user', (select json_object('userId', u.userId, 'name', u.name, 'email', u.email, 'role', u.role) from users u where u.userId = tm.userId)
+		)) from teamMembers tm where tm.teamId = t.teamId)
+	)) as data from teams t`
 				),
 				d1.first,
 				Effect.flatMap(Schema.decodeUnknown(TeamsResult))
