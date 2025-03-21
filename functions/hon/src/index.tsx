a74cab58a56725c0744bfdce281831010d577a6c
Michael Wu <mw10013@gmail.com>
2025-03-21 16:43:23 -0400

    downgrade cf vite plugin

--- functions/hon/src/index.tsx
+++ functions/hon/src/index.tsx
@@ -177,7 +177,7 @@ const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
 							</ul>
 						</div>
 						<a href="/" className="btn btn-ghost text-xl">
-							Hon v0.3
+							Hon v0.5
 						</a>
 					</div>
 					<div className="navbar-center hidden lg:flex">
