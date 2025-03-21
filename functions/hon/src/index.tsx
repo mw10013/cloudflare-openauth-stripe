259f6dbc459468649931f157b6db2983b25c6641
Michael Wu <mw10013@gmail.com>
2025-03-21 13:07:25 -0400

    v0.2

--- functions/hon/src/index.tsx
+++ functions/hon/src/index.tsx
@@ -177,7 +177,7 @@ const Layout: FC<PropsWithChildren<{}>> = ({ children }) => {
 							</ul>
 						</div>
 						<a href="/" className="btn btn-ghost text-xl">
-							Hon v0.1
+							Hon v0.2
 						</a>
 					</div>
 					<div className="navbar-center hidden lg:flex">
