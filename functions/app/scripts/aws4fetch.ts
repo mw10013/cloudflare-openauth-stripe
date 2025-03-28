83e6b9621bc854ff6a690532e7f34c6054eed281
Michael Wu <mw10013@gmail.com>
2025-03-28 18:41:44 -0400

    email

--- functions/app/scripts/aws4fetch.ts
+++ functions/app/scripts/aws4fetch.ts
@@ -1,36 +1,39 @@
 import { AwsClient } from 'aws4fetch'
 
+// https://www.npmjs.com/package/@aws-sdk/client-sesv2
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/sesv2/
+// https://docs.aws.amazon.com/ses/latest/APIReference-V2/Welcome.html
+// https://aws.amazon.com/blogs/messaging-and-targeting/upgrade-your-email-tech-stack-with-amazon-sesv2-api/
+// https://docs.aws.amazon.com/ses/latest/dg/Welcome.html
+// https://docs.aws.amazon.com/ses/latest/APIReference/Welcome.html
+// https://docs.aws.amazon.com/general/latest/gr/ses.html
+
 const aws = new AwsClient({
 	accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
 	region: 'us-east-1',
 	retries: 1
 })
+
 let resp = await aws.fetch('https://email.us-east-1.amazonaws.com/v2/email/outbound-emails', {
 	method: 'POST',
 	headers: {
 		'content-type': 'application/json'
 	},
 	body: JSON.stringify({
+		FromEmailAddress: 'motio@mail.com',
 		Destination: {
 			ToAddresses: ['motio1@mail.com']
 		},
-		FromEmailAddress: 'motio@mail.com',
 		Content: {
 			Simple: {
 				Subject: {
-					Data: 'This is aws4fetch'
+					Data: 'This is aws4fetch with ses v2'
 				},
 				Body: {
 					Text: {
 						Data: 'This is aws4fetch email.'.replace(/<br\s*[\/]?>/gi, '\n')
 					}
-					// Html: {
-					// 	Data:
-					// 		'<body><div align="center" style="font-family:Calibri, Arial, Helvetica, sans-serif;"><table width="600" cellpadding="0" cellspacing="0" border="0" style="font-family:Calibri, Arial, Helvetica, sans-serif"><tr style="background-color:white;"><td><table width="600" cellpadding="0"><tr><td><h1>Fifty Pence Direct Debit</h1><p>' +
-					// 		message +
-					// 		'</p></td></tr></table></td></tr></table></div></body>'
-					// }
 				}
 			}
 		}
