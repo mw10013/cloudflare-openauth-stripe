f25c4fbc448265a79d6661b9719bf1f51d105a4e
Michael Wu <mw10013@gmail.com>
2025-03-27 21:34:53 -0400

    ses

---
+++ functions/ses/src/SchemaEx.ts
@@ -0,0 +1,16 @@
+import { Schema } from 'effect'
+
+export const FormDataFromSelf = Schema.instanceOf(FormData).annotations({ identifier: 'FormDataFromSelf' })
+export const RecordFromFormData = Schema.transform(FormDataFromSelf, Schema.Record({ key: Schema.String, value: Schema.String }), {
+	strict: false,
+	decode: (formData) => Object.fromEntries(formData.entries()),
+	encode: (data) => {
+		const formData = new FormData()
+		for (const [key, value] of Object.entries(data)) {
+			formData.append(key, value)
+		}
+		return formData
+	}
+}).annotations({ identifier: 'RecordFromFormData' })
+export const FormDataSchema = <A, I extends Record<string, string>, R>(schema: Schema.Schema<A, I, R>) =>
+	Schema.compose(RecordFromFormData, schema, { strict: false })
