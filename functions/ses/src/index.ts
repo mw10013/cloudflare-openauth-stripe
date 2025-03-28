4f750b0882f897b28d9f99fd117c62de01d084d6
Michael Wu <mw10013@gmail.com>
2025-03-27 22:07:29 -0400

    fs

--- functions/ses/src/index.ts
+++ functions/ses/src/index.ts
@@ -1,5 +1,8 @@
+import { readFile } from 'fs'
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		readFile()
 		return new Response('Hello, world!')
 	}
 } satisfies ExportedHandler<Env>
